61cb8a8815202a28b4ac96b8c61396b0e139b9f7
Luis <leip1493@gmail.com>
2018-10-15 13:17:36 -0430

    Estados ascensor

--- Arduino web/sertecinweb/web/js/ajaxRead.js
+++ Arduino web/sertecinweb/web/js/ajaxRead.js
@@ -6,7 +6,8 @@ $(function(){
         method: "get",
         dataType: 'json',
         success: function(data){
-          mensaje(data);
+          const estado = Number(data.command);
+          estadoAscensor(estado);
         },
         error: function(jqXHR, textStatus, errorThrown){
           alert(errorThrown);
@@ -20,45 +21,117 @@ $(function(){
    },1000);
 });
 
-function mensaje(data){
-  switch ( Number(data.data) ) {
+function estadoAscensor(estado){
+  if (estado >= 0 && estado <= 3) {
+    estadoPB(estado);
+  }
+  else if (estado >= 10 && estado <= 13) {
+    estadoP1(estado);
+  }
+  else if (estado >= 20 && estado <= 23) {
+    estadoP2(estado);
+  }
+  else if (estado == 50 || estado == 51) {
+    subiendoPiso(estado);
+  }
+  else if (estado == 61 || estado == 62) {
+    bajandoPiso(estado);
+  }
+  else if(estado == 255){
+    $('#piso').html(`Reiniciando...<i class="fa fa-cog fa-spin fa-fw"></i>`);
+    $('#puertas').html(`Reiniciando...<i class="fa fa-cog fa-spin fa-fw"></i>`);
+  }else{
+    $('#piso').text('Error!');
+    $('#puertas').text('Error!');
+  }
+}
+
+function estadoPB(estado){
+  switch ( estado ) {
     case 0:
-       $('h3#puertas').text('Reposando PB');
-       $('h3#piso').text('PB');
+      $('#piso').text('PB');
+      $('#puertas').text('Cerradas');
     break;
     case 1:
-       $('h3#puertas').text('Reposando P1');
-       $('h3#piso').text('1');
+      $('#piso').text('PB');
+      $('#puertas').text('Cerrando');
     break;
     case 2:
-       $('h3#puertas').text('Reposando P2');
-       $('h3#piso').text('2');
+      $('#piso').text('PB');
+      $('#puertas').text('Abriendo');
     break;
     case 3:
-       $('h3#puertas').text('Abriendo');
+      $('#piso').text('PB');
+      $('#puertas').text('Abiertas');
     break;
-    case 4:
-       $('h3#puertas').text('Cerrando');
-    break;
-    case 5:
-       $('h3#puertas').text('Cerradas');
-       $('h3#piso').text('Subiendo');
-    break;
-    case 6:
-       $('h3#puertas').text('Cerradas');
-       $('h3#piso').text('Bajando');
-    break;
-    case 7:
-       $('h3#puertas').text('Config Subir');
-    break;
-    case 8:
-       $('h3#puertas').text('Config Bajar');
-    break;
-    case 9:
-       $('h3#puertas').text('Parando');
-    break;
+  }
+}
+
+function estadoP1(estado){
+  switch ( estado ) {
     case 10:
-       $('h3#puertas').text('Sensando');
+      $('#piso').text('P1');
+      $('#puertas').text('Cerradas');
     break;
-    }
+    case 11:
+      $('#piso').text('P1');
+      $('#puertas').text('Cerrando');
+    break;
+    case 12:
+      $('#piso').text('P1');
+      $('#puertas').text('Abriendo');
+    break;
+    case 13:
+      $('#piso').text('P1');
+      $('#puertas').text('Abiertas');
+    break;
+
+  }
+}
+
+function estadoP2(estado){
+  switch ( estado ) {
+    case 20:
+      $('#piso').text('P2');
+      $('#puertas').text('Cerradas');
+    break;
+    case 21:
+      $('#piso').text('P2');
+      $('#puertas').text('Cerrando');
+    break;
+    case 22:
+      $('#piso').text('P2');
+      $('#puertas').text('Abriendo');
+    break;
+    case 23:
+      $('#piso').text('P2');
+      $('#puertas').text('Abiertas');
+    break;
+  }
+}
+
+function subiendoPiso(estado){
+  switch (estado) {
+    case 50:
+      $('#piso').text('Subiendo a P1');
+      $('#puertas').text('Cerradas');
+    break;
+    case 51:
+      $('#piso').text('Subiendo a P2');
+      $('#puertas').text('Cerradas');
+    break;
+  }
+}
+
+function bajandoPiso(estado){
+  switch (estado) {
+    case 62:
+      $('#piso').text('Bajando a P1');
+      $('#puertas').text('Cerradas');
+    break;
+    case 61:
+      $('#piso').text('Bajando a PB');
+      $('#puertas').text('Cerradas');
+    break;
+  }
 }
